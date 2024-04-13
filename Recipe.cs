27adb9a875b688b97bdad3fb96b7f63a24d7caba
Nikhil Saroop <ST10040092@vcconnect.edu.za>
2024-04-13 20:04:25 +0200

    completing the method in the recipe class, added comments and color to the text of the application, Initialize arrays based on the number of ingredients, Looped the   inputted details for each ingredient,validated loop for ingredient quantity,added an exit for the loop for number of gredients

--- Recipe.cs
+++ Recipe.cs
@@ -1,43 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ST10040092RecipeAppPROG6221
 {
 	internal class Recipe
 	{
+		// Private fields to store recipe details
+		private string[] ingredientStoredNames; // Array to store ingredient names
+		private double[] StoredIngredientQuantities; // Array to store ingredient quantities
+		private string[] StoredingredientUnits; // Array to store ingredient units
+		private string[] recordedIngredientsSteps; // Array to store recipe steps
+		private double[] originalIngredientQuantities; // Array to store original ingredient quantities (not currently used)
 
-		private string[] ingredientStoredNames;
-		private double[] StoredIngredientQuantities;
-		private string[] StoredingredientUnits;
-		private string[] recordedIngredientsSteps;
-		private double[] originalIngredientQuantities;
-
-
+		// Method to input recipe details
 		public void EnterRecipeDetails()
 		{
+			// Variable to store the number of ingredients
 			int NumberOFIngredientsCount;
+
+			// Input validation loop for the number of ingredients
 			while (true)
 			{
+				Console.ForegroundColor = ConsoleColor.White;
 				Console.WriteLine("Enter number of ingredients");
-
-
+				Console.ResetColor();
 
 				if (!int.TryParse(Console.ReadLine(), out NumberOFIngredientsCount))
 				{
-
-					Console.WriteLine("Invalid , error has occured  from input. Please enter a number for the number of ingredients.");
-
-
+					Console.ForegroundColor = ConsoleColor.Magenta;
+					Console.WriteLine("Invalid input. Please enter a number for the number of ingredients.");
+					Console.ResetColor();
 				}
 				else if (NumberOFIngredientsCount <= 0)
-
 				{
-
-					Console.WriteLine("Invalid , error has occured  from input.The number of ingredients must be a positive integer.");
-
+					Console.ForegroundColor = ConsoleColor.Magenta;
+					Console.WriteLine("Invalid input. The number of ingredients must be a positive integer.");
+					Console.ResetColor();
 				}
 				else
 				{
@@ -47,25 +44,43 @@ namespace ST10040092RecipeAppPROG6221
 
 
 
+			// Initialize arrays based on the number of ingredients
+			ingredientStoredNames = new string[NumberOFIngredientsCount];
+			StoredIngredientQuantities = new double[NumberOFIngredientsCount];
+			StoredingredientUnits = new string[NumberOFIngredientsCount];
+
+			// Loop to input details for each ingredient
+			for (int D = 0; D < NumberOFIngredientsCount; D++)
+			{
+				Console.WriteLine($"\n Please enter details for ingredient {D + 1}:");
+				Console.Write("Name of Ingredient: ");
+				ingredientStoredNames[D] = Console.ReadLine();
+
+				// Input validation loop for ingredient quantity
+				while (true)
+				{
+					Console.Write("Quantity of ingredients: ");
+					if (!double.TryParse(Console.ReadLine(), out double measurementQauntity))
+					{
+						Console.ForegroundColor = ConsoleColor.Magenta;
+						Console.WriteLine("Invalid input. Please enter a numeric value for quantity.");
+						Console.ResetColor();
+					}
+					else if (measurementQauntity <= 0)
+					{
+						Console.ForegroundColor = ConsoleColor.Magenta;
+						Console.WriteLine("Invalid input. Quantity must be a positive number.");
+						Console.ResetColor();
+					}
+					else
+					{
+						StoredIngredientQuantities[D] = measurementQauntity;
+						break; // Exit the loop if input is valid
+					}
+				}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+			}
 		}
 	}
 }
